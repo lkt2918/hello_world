7741d2f4509fc811d626feca4e66d2e8395a25d4
lkt2918 <32476469+lkt2918@users.noreply.github.com>
2021-02-09 00:52:19 -0600

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,9 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
   });
 
-app.get('/', (req, res) => {
-    res.send('server is ready');
-});
+//app.get('/', (req, res) => {
+//    res.send('server is ready');
+//});
 //error catcher 
 app.use((err, req, res, next) =>{
     res.status(500).send({ message: err.message});
@@ -51,6 +51,11 @@ app.use((err, req, res, next) =>{
 //retrun current folder name 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 const port = process.env.PORT || 5000;
 app.listen(port, () =>{
     console.log (`Server at http://localhost:${port}`);
